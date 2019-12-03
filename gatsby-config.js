3fe67605a117e9e78b5ad2cb3e27fc319f911d77
devgeetech <devgeetech@gmail.com>
2019-12-03 10:46:36 +0530

    website icon update

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: "Providence Design Contest",
+        name: "Providence Design Competition",
         short_name: "PRODESCOM",
         start_url: "/",
         background_color: "#000000",
@@ -31,7 +31,7 @@ module.exports = {
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: "standalone",
-        icon: "content/logos/rasBNW.webp", // This path is relative to the root of the site.
+        icon: "content/logos/prodesBl.png", // This path is relative to the root of the site.
         // An optional attribute which provides support for CORS check.
         // If you do not provide a crossOrigin option, it will skip CORS for manifest.
         // Any invalid keyword or empty string defaults to `anonymous`
